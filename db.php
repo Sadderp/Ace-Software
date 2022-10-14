882970a2daa9a5b9a082104219202dbb6368f304
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-10-14 13:58:10 +0200

    all pages now give an error if 'POST' is used

--- db.php
+++ db.php
@@ -8,6 +8,10 @@ $dbname = "the_provider_db";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("Error - Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  die("Error: 'POST'-requests are not supported for our services.");
 }
 ?>
