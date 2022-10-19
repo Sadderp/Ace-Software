2d67510a698f2fa42594094838486d38090d6e06
Your NameCewin <cewin.eriksson@elev.ga.ntig.se>
2022-10-19 13:02:48 +0200

    Visa händelser, nästan med datum

--- Calendar/visa_date.php
+++ Calendar/visa_date.php
@@ -3,17 +3,18 @@
 
     $db = $conn;
     
-    $title = $_GET['title'];
-    
+    $date = $_GET['date'];
+    $End_date = $_GET['End_date'];
 
-    $sel = "SELECT * FROM calendar_event WHERE title=?";
+    $sel = "SELECT * FROM calendar_event WHERE (date>? AND End_date<?) OR (date<? OR End_date>?) OR (date<? AND End_date>?)";
 
     $stmt = $conn->prepare($sel);
-    $stmt->bind_param("s", $title);
+    $stmt->bind_param("ss", $date, $End_date);
     $stmt->execute();
     $result = $stmt->get_result();
     
-    if ($_GET['title'] == NULL){
+    // || $_GET['End_date']
+    if ($_GET['date'] == NULL ){
         $sel = "SELECT * FROM calendar_event";
         $selfraga = $db->query($sel);
         if ($selfraga->num_rows > 0) {
