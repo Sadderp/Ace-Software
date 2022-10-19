f89dc302775c1667fdf42478ab5ab85666d91c63
spookiebruh <zackeboibeat@gmail.com>
2022-10-19 09:59:38 +0200

    kan lägga in wiki i service tabellen via wiki_name

--- Wiki/create_wiki.php
+++ Wiki/create_wiki.php
@@ -3,20 +3,65 @@
     require_once("../db.php");
 
     // Gets user data
+   
+    
+
+    // Prepares statement
+    $stmt = $conn->prepare("INSERT INTO service (title, type) VALUES (?, ?)");
+    $stmt->bind_param("ss", $wiki_name, $type_wiki); 
+    
+    $stmt2 = $conn->prepare("INSERT INTO end_user (userID, serviceID) VALUES (?, ?)");
+    $stmt2->bind_param("ii", $userid, $wikiid); 
+    
+    $stmt3 = $conn->prepare("SELECT ID FROM user WHERE username = ? AND password = ?");
+    $stmt3->bind_param("ss", $user, $pass);  
+
+    $stmt4 = $conn->prepare("SELECT ID FROM service WHERE title = ?");
+    $stmt4->bind_param("s", $wiki_name);  
 
 
-    // Creates wiki
+    // Creates wiki variables
     $wiki_name = $_GET['wiki_name'];
+    $type_wiki = 'wiki';
 
+    // If statement to add the wiki to the database and show name after it gets added
     if(!empty($_GET['wiki_name'])) {
-        $sql = "INSERT INTO service (title, type) VALUES ('$wiki_name', 'wiki')";
+        $stmt->execute();
 
-        if ($conn->query($sql) === TRUE) {
-            echo "Created wiki ". $wiki_name."!";
-          } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-          }
+        if ($stmt->affected_rows == 1) {
+            echo json_encode($wiki_name);        
+        } else {
+            echo "Error: " . $stmt . "<br>" . $conn->error;
+        }
+
+        $user = "root";
+        $pass = "root";
+
+        $stmt3->execute();
+        $resultuid = $stmt3->get_result();
+        
+        if($resultuid->num_rows == 1){
+            $userid = $resultuid->fetch_array();
+        }
+
+        $stmt4->execute();
+        $resultsid = $stmt4->get_result();
+
+        if($resultsid->num_rows == 1){
+            $wikiid = $resultsid->fetch_assoc();
+        }
+        
+        $stmt2->execute();
+        
 
     }
-?>
+    
 
+
+
+    $stmt3->close();
+    $stmt4->close();
+    $stmt->close();
+    $stmt2->close();
+    $conn->close();
+?>
