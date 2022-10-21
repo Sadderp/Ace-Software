272858b22257e19a1d6833e9c73112e0402024c5
Albin Andersson <albin.andersson6@elev.ga.ntig.se>
2022-10-21 11:50:11 +0200

    fixat user i blogg

--- login.php
+++ login.php
@@ -22,27 +22,27 @@ if(!empty($_GET['name']) && !empty($_GET['password'])) {
     $stmt->execute();
     $result = $stmt->get_result();
 
+    $_SESSION['user_username'] = $name;
+    $_SESSION['password'] = $password;
+
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['password'] = $row['password'];
             if(password_verify($password, $row['password'])) {
                 if($row['admin'] == 1) {
                     // admin
                 } else if($row['ban'] == 0) {
                     // normal user
-                    echo $_SESSION['username'];
+                    echo $_SESSION['user_username'];
                     echo $_SESSION['password'];
                 } else {
                     // is banned and is not an admin
                 }
-            } else {
-                // not a user
             }
         }
     } 
     else {
         // not a user
+        echo ("NOT A USER!");
     }
 }
 
