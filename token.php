24505682b6e6b425eb92ae9c0d600ed21d5d3bad
Albin0825 <albinkarlsson2003@gmail.com>
2022-10-24 15:38:54 +0200

    can search for wiki pages & tokens is using prepar

--- token.php
+++ token.php
@@ -11,33 +11,42 @@ $version = "0.0.1";
 
 
 //==================================================
-// Looks what you have filled in
+// What time it is rn
 //==================================================
-$time = date('h:i:s', time());
+$time = date('H:i:s', time());
+
+
+
+//==================================================
+// Array with all the users IDs
+//==================================================
 $list = [];
 
 
 
-$sql = "SELECT id,token, end_date FROM user";
-$result = $conn->query($sql);
+//==================================================
+// Looks if all users tokens is up to date
+//==================================================
+$stmt = $conn->prepare("SELECT id,token, end_date FROM user");
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         $list[] = $row;
     }
-} else {
-    echo "0 results";
 }
-
 
 
 foreach($list as $x) {
     if($time >= $x['end_date']) {
-        $new_time = (intval($time)+1)*10000;
+        $new_time = ((intval($time)+1) % 24)*10000;
         $new_token = bin2hex(random_bytes(32));
-        $sql = "UPDATE user SET token = '$new_token', end_date = '$new_time' WHERE id=$x[id]";
-        $result = $conn->query($sql);
+
+        $stmt = $conn->prepare("UPDATE user SET token=?, end_date=? WHERE id=?");
+        $stmt->bind_param("ssi", $new_token, $new_time, $x['id']);
+        $stmt->execute();
     }
 }
 
