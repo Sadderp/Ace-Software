36a0aba52173302139f1e62df760a02ee2ce4aa6
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-10-25 16:18:49 +0200

    0.0.2 - End-user permissions are now required to see the history of a page

--- Wiki/wiki_get_history.php
+++ Wiki/wiki_get_history.php
@@ -2,13 +2,14 @@
     require_once("../db.php");
     require_once("../utility.php");
     require_once("wiki_get_version.php");
-    $v = "0.0.1";
+    require_once("get_wiki_from_page.php");
+    $v = "0.0.2";
 
     /**
      * wiki_get_history.php
      * 
      * Get all the version and content data of a wiki page.
-     * <TODO> Only accessible to wiki end users
+     * Only accessible to wiki end users
      */
 
     //==============================
@@ -22,9 +23,18 @@
     //    Get variables
     //==============================
     $page_id = get_if_set('page_id');
+    $user_id = get_if_set('user_id');
 
-    if(!$page_id) {
-        error_message($v,"Missing input(s) - expected: 'page_id'");
+    if(!$page_id or !$user_id) {
+        error_message($v,"Missing input(s) - expected: 'page_id' and 'user_id'");
+    }
+
+    //==============================
+    //    Check user permissions
+    //==============================
+    $wiki_id = get_wiki_from_page($page_id);
+    if(!check_end_user($user_id,$wiki_id)) {
+        error_message($v,"User does not have permission to view the history of this page");
     }
 
     //==============================
