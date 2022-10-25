8e7bb01bfa3a88b58b42b0ecb4dc51d658e31b7f
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-10-25 14:12:39 +0200

    A function for getting the most recent version number of a wiki page

---
+++ Wiki/wiki_get_recent_version.php
@@ -0,0 +1,35 @@
+<?php
+    require_once("../db.php");
+    require_once("../utility.php");
+
+    /**
+     * get_recent_version
+     * 
+     * Get the most recent version number for a page
+     *
+     * @param    int  $page_id      The ID of the page
+     * @return   int 
+     *
+     */
+    function get_recent_version($page_id) {
+
+        //==============================
+        //    Prepared Statement
+        //==============================
+        $sql = "SELECT MAX(num) AS 'version_num' FROM wiki_page_version WHERE pageID = ?";
+        $stmt_get_version = $GLOBALS['conn']->prepare($sql);
+        $stmt_get_version->bind_param("i",$page_id);
+
+        //==============================
+        //    Get the most recent version
+        //==============================
+        $stmt_get_version->execute();
+        $version = mysqli_fetch_assoc($stmt_get_version->get_result())['version_num'];
+
+        // Default to 0 if no version has been made yet.
+        if(!$version) {
+            return 0;
+        }
+        return $version;
+    }
+?>
