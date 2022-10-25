cd602c095555e0ad9c9a4e31dbc881c97cc9e8b1
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-10-25 11:01:02 +0200

    0.0.2 - Check if user has wiki privileges when creating page

--- Wiki/wiki_create_page.php
+++ Wiki/wiki_create_page.php
@@ -1,41 +1,58 @@
 <?php
     require_once("../db.php");
     require_once("../utility.php");
-    $version = "0.0.2";
+    $v = "0.0.2";
 
     // ! ! THIS PAGE IS MISSING USER VERIFICATION ! !
 
     //==============================
     //    Prepared statements
     //==============================
-    $stmt = $conn->prepare("INSERT INTO wiki_page (serviceID, title) VALUES (?,?)");
-    $stmt->bind_param("is",$wiki_id,$page_title);
+
+    // Create wiki page
+    $sql = "INSERT INTO wiki_page (serviceID, title) VALUES (?,?)";
+    $stmt_new_page = $conn->prepare($sql);
+    $stmt_new_page->bind_param("is",$wiki_id,$page_title);
+
+    // Check if user has wiki privileges
+    $sql = "SELECT * FROM end_user WHERE userID = ? AND serviceID = ?";
+    $stmt_check_perms = $conn->prepare($sql);
+    $stmt_check_perms->bind_param("ii",$user_id,$wiki_id);
     
     //==============================
     //    Get variables
     //==============================
     $wiki_id = get_if_set('wiki_id');
+    $user_id = get_if_set('user_id');
     $page_title = get_if_set('page_title');
 
     // Give error message if one or more inputs are blank
-    if(!$wiki_id or !$page_title) {
-        $result = ["version"=>$version, "status"=>"ERROR", "data"=>"Missing input - expected: 'wiki_id' and 'page_title'"];
-        die(json_encode($result));
+    if(!$wiki_id or !$user_id or !$page_title) {
+        error_message($v,"Missing input - expected: 'wiki_id', 'user_id' and 'page_title'");
     }
 
     //==============================
-    //    Create page and return status
+    //    Check user permissions in wiki
     //==============================
-    $stmt->execute();
+    $stmt_check_perms->execute();
+    if($stmt_check_perms->get_result()->num_rows == 0) {
+        error_message($v,"User does not have permission to edit this page");
+    }
+
+    //==============================
+    //    Create page
+    //==============================
+    $stmt_new_page->execute();
     
     // Check if operation is successful
-    if($stmt->affected_rows === 1) {
-        $result = ["version"=>$version, "status"=>"OK", "data"=>$page_title];
+    if($stmt_new_page->affected_rows === 1) {
+        $result = ["version"=>$v, "status"=>"OK", "data"=>$page_title];
     } else {
-        $result = ["version"=>$version, "status"=>"ERROR", "data"=>"Failed to add to database"];
+        error_message($v,"Failed to add to database");
     }
     
     echo json_encode($result);
 
-    $stmt->close();
+    $stmt_new_page->close();
+    $stmt_check_perms->close();
 ?>
