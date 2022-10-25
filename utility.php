a4572ba82260dbd0b2167b2188ae82cf26359827
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-10-25 10:57:22 +0200

    new error_message() utility function, and some more documentation

--- utility.php
+++ utility.php
@@ -1,13 +1,29 @@
 
 <?php
 
-    // If the given $_GET-variable exists, return it. Else return false.
-    // Usage: $user_id = get_if_set('user_id');
-    function get_if_set($data) {
-        if(isset($_GET[$data])) {
-            return $_GET[$data];
+    /**
+    * Check a $_GET variable and return its value if it has one. Else returns false.
+    *
+    * @param    string  $variable   The variable name to be checked   
+    *
+    */
+    function get_if_set($variable) {
+        if(isset($_GET[$variable])) {
+            return $_GET[$variable];
         } else {
             return false;
         }
+    }
+
+    /**
+    * Give a JSON error message and exit the program
+    *
+    * @param    string  $version    The program version
+    * @param    string  $msg        Error message
+    *
+    */
+    function error_message($version,$msg) {
+        $result = ["version"=>$version, "status"=>"ERROR", "data"=>$msg];
+        die(json_encode($result));
     }
 ?>
