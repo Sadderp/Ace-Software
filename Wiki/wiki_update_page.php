d339e21942d140204a8fc083df8660f9c9161c96
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-10-25 11:03:27 +0200

    0.0.3 - Proper JSON return + Check if user has wiki privileges before adding content

--- Wiki/wiki_update_page.php
+++ Wiki/wiki_update_page.php
@@ -1,36 +1,44 @@
 <?php
     require_once("../db.php");
     require_once("../utility.php");
-    $version = "0.0.1";
+    $v = "0.0.3";
 
     // TEST LINK:
-    // http://localhost:8080/webbutveckling/TE4/Ace-Software/wiki/wiki_update_page.php?user_id=1&page_id=1&content=["<h1>Joe Biden</h1>","<p>Joe Biden is the preseident of the United States of America</p>"]
+    // http://localhost:8080/webbutveckling/TE4/Ace-Software/wiki/wiki_update_page.php?user_id=1&page_id=1&content=["<h1>RobTop</h1>","<p>RobTop is the lead developer of Geometry Dash</p>"]
 
-
-
-
-
-    // ! ! THIS PAGE IS MISSING USER VERIFICATION ! !
+    // THIS PAGE IS MISSING USER TOKEN VERIFICATION ! !
+    // DON'T KNOW HOW TO DO THAT SHIT ! !
 
     //==============================
     //    Prepared statements
     //==============================
 
     // Get current version
-    $sql1 = "SELECT MAX(num) AS version_num FROM wiki_page_version WHERE pageID = ?";
-    $stmt1 = $conn->prepare($sql1);
-    $stmt1->bind_param("i",$page_id);
+    $sql = "SELECT MAX(num) AS 'version_num' FROM wiki_page_version WHERE pageID = ?";
+    $stmt_get_version = $conn->prepare($sql);
+    $stmt_get_version->bind_param("i",$page_id);
 
     // Create new version
-    $sql2 = "INSERT INTO wiki_page_version (pageID,num,userID,date) VALUES (?,?,?,now())";
-    $stmt2 = $conn->prepare($sql2);
-    $stmt2->bind_param("iii",$page_id,$new_version,$user_id);
-
+    $sql = "INSERT INTO wiki_page_version (pageID,num,userID,date) VALUES (?,?,?,now())";
+    $stmt_add_version = $conn->prepare($sql);
+    $stmt_add_version->bind_param("iii",$page_id,$new_version,$user_id);
 
     // Add content
-    $sql3 = "INSERT INTO content (pageID,versionID,contents) VALUES (?,?,?)";
-    $stmt3 = $conn->prepare($sql3);
-    $stmt3->bind_param("iis",$page_id,$new_version,$content);
+    $sql = "INSERT INTO content (pageID,versionID,contents) VALUES (?,?,?)";
+    $stmt_add_content = $conn->prepare($sql);
+    $stmt_add_content->bind_param("iis",$page_id,$new_version,$content);
+
+    // Get wiki from page
+    $sql = "SELECT service.ID AS 'wikiID', service.type FROM wiki_page 
+        LEFT JOIN service ON service.ID = wiki_page.serviceID 
+        WHERE wiki_page.ID = ?";
+    $stmt_get_wiki = $conn->prepare($sql);
+    $stmt_get_wiki->bind_param("i",$page_id);
+
+    // Check if user has wiki privileges
+    $sql = "SELECT * FROM end_user WHERE userID = ? AND serviceID = ?";
+    $stmt_check_perms = $conn->prepare($sql);
+    $stmt_check_perms->bind_param("ii",$user_id,$wiki_id);
 
     //==============================
     //    Get variables
@@ -41,15 +49,14 @@
 
     // Give error message if one or more inputs are blank
     if(!$user_id or !$page_id) {
-        $result = ["version"=>$version, "status"=>"ERROR", "data"=>"Missing input(s) - expected: 'user_id', 'page_id' and 'content'"];
-        die(json_encode($result));
+        error_message($v,"Missing input(s) - expected: 'user_id', 'page_id' and 'content'");
     }
 
     //==============================
     //    Get current version number
     //==============================
-    $stmt1->execute();
-    $current_version = mysqli_fetch_assoc($stmt1->get_result())['version_num'];
+    $stmt_get_version->execute();
+    $current_version = mysqli_fetch_assoc($stmt_get_version->get_result())['version_num'];
     if(!$current_version) {
         $current_version = 0;
     }
@@ -58,21 +65,47 @@
     //==============================
     //    Get wiki ID
     //==============================
-    //$stmt3->execute();
-    //$wiki_id = mysqli_fetch_assoc($stmt1->get_result())['wiki'];
+    $stmt_get_wiki->execute();
+    $result = $stmt_get_wiki->get_result();
+    $r = mysqli_fetch_assoc($result);
+
+    if($result->num_rows == 0 or $r['type'] != "wiki") {
+        error_message($v,"Wiki page not found");
+    }
+
+    $wiki_id = $r['wikiID'];
 
     //==============================
-    //    idk
+    //    Check user permissions in wiki
+    //==============================
+    $stmt_check_perms->execute();
+    if($stmt_check_perms->get_result()->num_rows == 0) {
+        error_message($v,"User does not have permission to edit this page");
+    }
+
+    //==============================
+    //    Add new version and content to database
     //==============================
     $content_array = json_decode($content_array);
 
-    $stmt2->execute();
+    $stmt_add_version->execute();
 
     foreach($content_array as $c) {
         $content = $c;
-        $stmt3->execute();
+        $stmt_add_content->execute();
     }
 
-    $stmt1->close();
-    $stmt2->close();
+    if($stmt_add_version->affected_rows == 1 and $stmt_add_content->affected_rows >= 1) {
+        $result = ["version"=>$v, "status"=>"OK", "data"=>"Successfully updated wiki page (v" . $new_version . ")"];
+    } else {
+        error_message($v,"Failed to add to database");
+    }
+
+    echo json_encode($result);
+
+    $stmt_get_version->close();
+    $stmt_get_wiki->close();
+    $stmt_check_perms->close();
+    $stmt_add_version->close();
+    $stmt_add_content->close();
 ?>
