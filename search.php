7e7096c5fba68d8486dbb019e4f4102421f8d32c
spookiebruh <zackeboibeat@gmail.com>
2022-10-26 10:39:37 +0200

    fixed search

--- search.php
+++ search.php
@@ -17,14 +17,16 @@ if(!empty($_GET['type']) && !empty($_GET['title'])) {
   $type = $_GET['type'];
   $title = "%".$_GET['title']."%";
 
-  $stmt = $conn->prepare("SELECT * FROM wiki_page WHERE serviceID=? AND title LIKE ?");
+  $stmt = $conn->prepare("SELECT * FROM service WHERE type=? AND title LIKE ?");
   $stmt->bind_param("ss", $type, $title);
+  $stmt->execute();
 }
 else if(!empty($_GET['type'])) {
   $type = $_GET['type'];
 
-  $stmt = $conn->prepare("SELECT * FROM wiki_page WHERE serviceID=?");
+  $stmt = $conn->prepare("SELECT * FROM service WHERE type=?");
   $stmt->bind_param("s", $type);
+  $stmt->execute();
 }
 //==================================================
 // Wiki Page
