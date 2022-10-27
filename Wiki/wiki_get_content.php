7af317d36be602f88b89a1555d7f0654b2b22a83
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2022-10-27 13:45:20 +0200

    Fixed file paths

--- Wiki/wiki_get_content.php
+++ Wiki/wiki_get_content.php
@@ -1,8 +1,8 @@
 <?php
     require_once("../db.php");
     require_once("../utility.php");
-    require_once("wiki_get_version.php");
-    require_once("wiki_get_recent_version.php");
+    require_once("Functions/wiki_get_version.php");
+    require_once("Functions/wiki_get_recent_version.php");
     $version = "0.0.2";
 
     /**
