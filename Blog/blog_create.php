55ef2d97e3e96642d961130d5af4ff2142ef05b6
Albin Andersson <albin.andersson6@elev.ga.ntig.se>
2022-10-27 14:07:13 +0200

    blog_post_create works correctly

--- Blog/blog_create.php
+++ Blog/blog_create.php
@@ -20,28 +20,28 @@ if (!empty($_GET['title'])&& !empty($_GET['user']) && !empty($_GET['token'])){
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             $userID = $row['Uid'];
-            if($row['token'] == $_GET['token']) {
-                if($row['username'] == $_GET['user']){
-                    $sql = "INSERT INTO service(title,type) VALUES (?,'blog')";
-                    $stmt = $conn->prepare($sql);
-                    $stmt->bind_param("s",$title); 
-                    $stmt->execute();
-                    printf($lastID = $conn->insert_id); 
-
-
-                    $sql2= "INSERT INTO end_user(userID,serviceID) VALUES (?,?)";
-                    $stmt2 = $conn->prepare($sql2);
-                    $stmt2->bind_param("ii",$userID,$lastID); 
-                    $stmt2->execute();
-                    $json_array = ["Version: "=>$version,"Status: "=>$ok,"Data: "=>'Blog was created successfully'];
-                    echo json_encode($json_array);
-                }else{
-                    $json_array = ["Version: "=>$version,"Status: "=>$error,"Data: "=>'Access denied!'];
-                    echo json_encode($json_array);
-                }
-            }else{
+            if($row['token'] != $_GET['token']) {
                 $json_array = ["Version: "=>$version,"Status: "=>$error,"Data: "=>'Access denied!'];
                 echo json_encode($json_array);
+                die();
+            }
+            if($row['username'] != $_GET['user']){
+                $json_array = ["Version: "=>$version,"Status: "=>$error,"Data: "=>'Access denied!'];
+                echo json_encode($json_array);
+            }
+            $sql = "INSERT INTO service(title,type) VALUES (?,'blog')";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s",$title); 
+            $stmt->execute();
+            printf($lastID = $conn->insert_id); 
+
+
+            $sql2= "INSERT INTO end_user(userID,serviceID) VALUES (?,?)";
+            $stmt2 = $conn->prepare($sql2);
+            $stmt2->bind_param("ii",$userID,$lastID); 
+            $stmt2->execute();
+            $json_array = ["Version: "=>$version,"Status: "=>$ok,"Data: "=>'Blog was created successfully'];
+            echo json_encode($json_array);
             }
         }
     } else {
@@ -49,7 +49,7 @@ if (!empty($_GET['title'])&& !empty($_GET['user']) && !empty($_GET['token'])){
         $json_array = ["Version: "=>$version,"Status: "=>$error,"Data: "=>'Access denied!'];
         echo json_encode($json_array);
     }
-} else{
+else{
     
     $json_array = ["Version: "=>$version,"Status: "=>$error,"Data: "=>'The URL is empty!'];
     echo json_encode($json_array);
