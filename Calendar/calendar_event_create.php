90d11a701c790ae7237184bdfdd1e14a11fbf44c
xXEmulsk69420Xx <emil.jonsson2@elev.ga.ntig.se>
2022-11-02 14:50:07 +0100

    strukturerad kod kinda

--- Calendar/calendar_event_create.php
+++ Calendar/calendar_event_create.php
@@ -1,71 +1,59 @@
 <?php
-    require_once("../db.php");
-    require_once("../verify_token.php");
-    $version = "0.0.8";
-    $ok = "OK";
-    $error = "Error";
+require_once("../db.php");
+require_once("../verify_token.php");
+require_once("../utility.php");
 
-    $db = $conn;
+//==================================================
+//      Get variables
+//==================================================
+$user_id = get_if_set('userID');
+$token = get_if_set('token');
 
-    if(!empty($_GET['userID'])&& !empty($_GET['token'])){
-        $userID = $_GET['userID'];
-        $token = $_GET['token'];
-    }else{
-        echo json_encode(["Version: "=>$version, "Type: "=>$error, "Data: "=>"You need to log in"]);
-    }
+$date = get_if_set('date');
+$end_date = get_if_set('end_date');
+$title = get_if_set('title');
+$description = get_if_set('description');
+//==================================================
+//      Requirements
+//==================================================
+if(!verify_token($user_id,$token)) {
+    output_error("Token is invalid or expired");
+}
+
+//==================================================
+//      Finds the user
+//==================================================
+$stmt = $conn->prepare("SELECT * FROM user WHERE ID=? AND token=?");
+$stmt->bind_param("is", $userID, $token);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    while($row = $result3->fetch_assoc()) {
+        $userID = $row['ID'];
+        }
+}else {
+    output_error("No user");
+}
+
+//==================================================
+//      Checks if start date is set before end date
+//==================================================
+if($date > $end_date){
+    output_error("You can not put the end date before the start date");
+}
+
+$stmt = $conn->prepare("INSERT INTO calendar_event(userID, date, end_date, title, description) VALUES (?,?,?,?,?)");
+$stmt->bind_param("issss", $userID, $date, $end_date, $title, $description);
+$stmt->execute();
+
     
-    verify_token($userID, $token);
-
-    $sql2 = "SELECT * FROM user WHERE ID=? AND token=?";
-
-    $statement = $conn->prepare($sql2);
-    $statement->bind_param("is", $userID, $token);
-    $statement->execute();
-    $result3 = $statement->get_result();
-
-    if ($result3->num_rows > 0) {
-        while($row = $result3->fetch_assoc()) {
-            $userID = $row['ID'];
-            }
-    }else {
-        echo json_encode("No user");
-    }
-
-    //Check info about the event
-    if(!empty($_GET['date'])){
-        $date = $_GET['date'];
-    };
-
-    if(!empty($_GET['end_date'])){
-        $end_date = $_GET['end_date'];
-    };
-
-    if(!empty($_GET['title'])){
-        $title = $_GET['title'];
-    };
-
-    if(!empty($_GET['description'])){
-        $description = $_GET['description'];
-    };
-    
-    $sql = "INSERT INTO calendar_event(userID, date, end_date, title, description) VALUES (?,?,?,?,?)";
-
-    if($date > $end_date){
-        $json_result = ["Version"=>$version, "Status"=>$ok, "Data"=>"You can not put the end date before the start date"];
-        echo json_encode($json_result);
-        die();
-    }
-
-    //prepared statement
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("issss", $userID, $date, $end_date, $title, $description);
-    $stmt->execute();
-
-    if ($stmt->affected_rows === 1) {
-        $json_result = ["Version"=>$version, "Status"=>$ok, "Data"=>"event created"];
-        echo json_encode($json_result);
-    } else {
-        $json_result = ["Version"=>$version, "Status"=>$error, "Data"=>"uh oh"];
-        echo json_encode($json_result);
-    }   
+//==================================================
+//      Skapar ett event om den kan
+//==================================================
+if ($stmt->affected_rows === 1) {
+    output_ok("Event created");
+} else {
+    output_error("Could not create an event");
+}   
 ?>
