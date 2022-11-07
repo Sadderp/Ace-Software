f60432c505e2723c874f37d477502c0f21cd106a
Albin0825 <albinkarlsson2003@gmail.com>
2022-11-07 09:45:07 +0100

    code is better structured

--- Calendar/calendar_event_list_date.php
+++ Calendar/calendar_event_list_date.php
@@ -3,75 +3,83 @@
     require_once("../verify_token.php");
     require_once("../utility.php");
     
-    $evdate = get_if_set('evdate');
-    $evend_date = get_if_set('evend_date');
+
+    $date = get_if_set('date');
+    $end_date = get_if_set('end_date');
+    if(strlen($date) >= 11 || strlen($end_date) >= 11) {
+        output_error("Date or end date is formatted wrong");
+    }
 
     $user_id = get_if_set('user_id');
     $token = get_if_set('token');
 
-    if(!$user_id && !$token){
-        output_ok("You need fill in user_id and token")
+
+    if(!$user_id || !$token || !$date || !$end_date){
+        output_error("You need to fill in user_id, token, date and end_date");
     }
 
     if(!verify_token($user_id, $token)){
         output_error("Token is invalid or expired");
     }
 
+
+
     //===============================
     //    Prepared statements
     //===============================
-    $sql = "SELECT * FROM user WHERE ID=? AND token=?";
-
-    $stmt = $conn->prepare($sql);
+    $stmt = $conn->prepare("SELECT * FROM user WHERE ID=? AND token=?");
     $stmt->bind_param("is", $user_id, $token);
-
-    $sql2 = "SELECT * FROM calendar_event INNER JOIN calendar_invite ON calendar_event.userID!=calendar_invite.userID
-    WHERE calendar_invite.userID=? AND calendar_event.ID=calendar_invite.eventID AND (end_date >= ? AND date <= ?)";
-
-    $stmt2 = $conn->prepare($sql2);
-    $stmt2->bind_param("iss",$user_id, $evdate, $evend_date);
-
-    $sql3 = "SELECT * FROM calendar_event WHERE user_id=? AND (end_date >= ? AND date <= ?)";
-
-    $stmt3 = $conn->prepare($sql3);
-    $stmt3->bind_param("iss", $user_id, $evdate, $evend_date);
-
-
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             $user_id = $row['ID'];
-            }
+        }
     }else {
         output_error("No user");
     }
 
-    $stmt2->execute();
-    $result2 = $stmt2->get_result();
 
-    $stmt3->execute();
-    $result3 = $stmt3->get_result();
 
     //===============================
     //    Lists your own events
     //===============================
-    if($result3->num_rows == 0){
-        output_error("You have to write a start and end date");
+    $stmt = $conn->prepare("SELECT * FROM calendar_event WHERE userID=? AND (end_date >= ? AND date <= ?)");
+    $stmt->bind_param("iss", $user_id, $date, $end_date);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows == 0){
+        $json_result[] = "Could not find any events";
     }
-    if ($result3->num_rows > 0) {
-        while($row = $result3->fetch_assoc()) {
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
             $json_result[] = "ID: ".$row["ID"]. " Date: ".$row["date"]. " End_date: ".$row["end_date"]. " Title: ".$row["title"]. " Description: ".$row["description"];
-            }
+        }
     }
+
+
+
     //===============================
     //    Lists invites to events
     //===============================
-    if ($result2->num_rows > 0) {
-        while($row = $result2->fetch_assoc()) {
+    $stmt = $conn->prepare("SELECT * FROM calendar_event INNER JOIN calendar_invite ON calendar_event.userID!=calendar_invite.userID
+    WHERE calendar_invite.userID=? AND calendar_event.ID=calendar_invite.eventID AND (end_date >= ? AND date <= ?)");
+    $stmt->bind_param("iss",$user_id, $date, $end_date);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
             $json_result[] = "Invited to:"." ID: ".$row["eventID"]." by: "." user_id: ".$row["user_id"]. " Date: ".$row["date"]. " End_date: ".$row["end_date"]. " Title: ".$row["title"]. " Description: ".$row["description"];
-            }
+        }
     }
+
+
+
+    //===============================
+    //    Output
+    //===============================
     output_ok($json_result);
 ?>
